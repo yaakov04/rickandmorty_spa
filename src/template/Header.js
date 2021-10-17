7454b62d2d907e9bdf1e98600ac8f61058f102a0
Yaakov <dbrionesmtz@yahoo.com>
2021-10-16 23:57:53 -0500

    agregando proyecto

---
+++ src/template/Header.js
@@ -0,0 +1,18 @@
+const Header = () => {
+    const view = `
+        <div class="Header-main">
+            <div>
+                <h1>
+                    <a href="/">Rick and Morty SPA</a>
+                </h1>
+            </div> 
+            <div class="Header-nav">
+                    <a href="#/about/">About</a>
+            </div>
+        </div> 
+    `;
+
+    return view;
+};
+
+export default Header;
